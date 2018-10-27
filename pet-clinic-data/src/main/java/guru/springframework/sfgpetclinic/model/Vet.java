1a80c533931c230297df5930136e4bf8e7a1858f
rajesh23-drg <rmanjunath@teamdrg.com>
2018-10-27 15:29:01 +0530

    Added specialities, added it to vet. Closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> speacilities;
+
+    public Set<Speciality> getSpeacilities() {
+        return speacilities;
+    }
+
+    public void setSpeacilities(Set<Speciality> speacilities) {
+        this.speacilities = speacilities;
+    }
 }
