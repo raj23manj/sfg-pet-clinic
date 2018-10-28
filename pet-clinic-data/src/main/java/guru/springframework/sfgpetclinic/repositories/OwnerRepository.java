6dfa0fe8c59f078adb7933a573f32300eedae3fe
rajesh23-drg <rmanjunath@teamdrg.com>
2018-10-28 19:19:53 +0530

    Implemented repos. Closes #39

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.CrudServices;
+
+public interface OwnerRepository extends CrudServices<Owner, Long> {
+}
