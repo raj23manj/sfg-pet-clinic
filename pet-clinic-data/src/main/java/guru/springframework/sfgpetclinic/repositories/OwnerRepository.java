efdda6a79e93e1281ad8bdca5e53df8ffdc97f95
rajesh23-drg <rmanjunath@teamdrg.com>
2018-10-28 19:48:05 +0530

    added spring data jpa service for owners. Closes #40

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -1,7 +1,8 @@
 package guru.springframework.sfgpetclinic.repositories;
 
 import guru.springframework.sfgpetclinic.model.Owner;
-import guru.springframework.sfgpetclinic.services.CrudServices;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OwnerRepository extends CrudServices<Owner, Long> {
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
