834a1ced33ee2b29163a44618743ba48b3c4bc7b
rajesh23-drg <rmanjunath@teamdrg.com>
2018-11-01 19:37:23 +0530

    Impl find Owner. Closes #54

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -17,8 +17,8 @@ public class PetType extends BaseEntity {
     @Column(name="name")
     private String name;
 
-    @Override
-    public String toString() {
-        return name;
-    }
+//    @Override
+//    public String toString() {
+//        return name;
+//    }
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,8 +2,10 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudServices<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
+    List<Owner> findAllByLastNameLike(String lastName);
 }
